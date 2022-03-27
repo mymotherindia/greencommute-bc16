081fb7a9d05513adcb89fbe07df221f23398fd0c
mymotherindia <praveensanjeev01@yahoo.com>
2022-03-27 14:25:01 +0530

    16th-commit

--- src/main/java/com/greencommute/jobservice/repository/JobRolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greencommute.jobservice.repository;
-
-import com.greencommute.jobservice.entity.JobRoles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface JobRolesRepository extends JpaRepository<JobRoles, UUID> {
-}
--- src/main/java/com/greencommute/jobservice/repository/SaveJobsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greencommute.jobservice.repository;
-
-import com.greencommute.jobservice.entity.SaveJobs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SaveJobsRepository extends JpaRepository<SaveJobs, UUID> {
-}
